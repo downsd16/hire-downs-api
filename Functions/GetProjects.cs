57bd00ca1256cb98df3ff479833db426f5780415
downsd16 <downsd16@gmail.com>
2023-02-03 14:44:42 -0500

    add first function

--- Functions/GetProjects.cs
+++ Functions/GetProjects.cs
@@ -1,63 +1,39 @@
 using Azure;
 using System;
-using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using Azure.Data.Tables;
 using Company.Function.Model;
 using Microsoft.Azure.WebJobs;
 using Microsoft.AspNetCore.Mvc;
+using Company.Function.Services;
 using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace MyFunctionApp
 {
-    public static class GetProjects
+    public class GetProjects
     {
+        private readonly IDataService _dataService;
+
+        public GetProjects(
+            IDataService dataService) 
+        {
+            _dataService = dataService;
+        }
+
         [FunctionName("GetProjects")]
-        public static IActionResult Run(
+        public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
             Microsoft.AspNetCore.Http.HttpRequest req)
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-
-            var tableClient = new TableClient(
-                new Uri(config["STORAGE_URI"]),
-                config["TABLE_NAME"],
-                new TableSharedKeyCredential(
-                    config["STORAGE_ACCOUNT_NAME"],
-                    config["STORAGE_ACCOUNT_KEY"]
-                ));
-
-            //Make query for 'projects' partition
-            Pageable<TableEntity> projectTableResults = tableClient
-                .Query<TableEntity>(
-                    ent => ent.PartitionKey == config["STORAGE_PROJECT_PARTITION"]
-                );
-
-            //Create Variables for Projects
+            Pageable<TableEntity> projectTableResults = _dataService.GetProjects();
             List<Project> projectResult = new List<Project>();
-            int rowKey = -1;
 
-            //Iterate over Table and assign parameters for Projects
             foreach (TableEntity qProjectEntity in projectTableResults)
             {
-                //Converting string RowKey to int
-                try{
-                    rowKey = Int32.Parse(qProjectEntity.GetString("RowKey"));
-                } catch (Exception e) {
-                    Console.WriteLine(e);
-                    return new BadRequestObjectResult("Error Converting RowKey (string) to RowKey (int)");
-                }
-
-                //Create new Project objects
                 projectResult.Add(new Project(
-                    rowKey, 
+                    Int32.Parse(qProjectEntity.GetString("RowKey")), 
                     qProjectEntity.GetString("Name"),
                     qProjectEntity.GetString("ImageURL"),
                     qProjectEntity.GetString("Description"),
@@ -67,11 +43,9 @@ namespace MyFunctionApp
                 ));
             }
 
-            // Serialize the results to a JSON string
-            string jsonProj = JsonConvert.SerializeObject(projectResult.OrderBy(ent => ent.RowKey).ToList());
-
-            // Return the JSON string as the response
-            return new OkObjectResult(jsonProj);
+            return new OkObjectResult(JsonConvert.SerializeObject(
+                projectResult.OrderBy(ent => ent.RowKey).ToList()
+            ));
         }
     }
 }
